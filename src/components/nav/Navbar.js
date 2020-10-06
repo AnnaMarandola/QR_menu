accbedeec6be9ddb83de46f8d31a9ea735233ee6
AnnaMarandola <annamarandola@gmail.com>
2020-10-06 17:50:54 +0200

    tracking auth status - display menu

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,46 +1,32 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LOGO from "../../assets/LogoProject.png";
 import "../../App.css";
-import { connect } from 'react-redux';
-import { signOut } from '../../store/actions/authActions';
+import { connect } from "react-redux";
+import Menu from '../nav/Menu';
 
 const Navbar = (props) => {
+  const { auth } = props;
+  const isLogged = auth.uid ? <Menu/> : null
+  console.log('authUid', auth.uid);
   return (
     <nav>
       <div className="navbar">
         <Link to="/">
           <img src={LOGO} alt="logo" className="logo" />
         </Link>
-        <ul>
-          <li>
-            <a onClick={props.signOut()} href="logout">Déconnexion</a>
-          </li>
-          <li>
-            <NavLink to="./myaccount">Mon compte</NavLink>
-          </li>
-          {/* <li>
-            <NavLink to="./signin">Log in</NavLink>
-          </li>           */}
-
-          {/*<li><NavLink to='./inforesto'>Ma carte en ligne</NavLink></li>
-        <li><NavLink to='./inforesto'>Modifier ma carte</NavLink></li> */}
-        </ul>
+    { isLogged  }
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log(state)
   return {
-  }
-}
+    auth: state.firebase.auth,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signOut: () => dispatch(signOut())
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+
+export default connect(mapStateToProps)(Navbar);
