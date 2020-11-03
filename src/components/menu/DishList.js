8724d594986e3d0f0ed3924790bbb2642d1440e0
AnnaMarandola <annamarandola@gmail.com>
2020-11-03 16:57:44 +0100

    dishes css for menupage

--- src/components/menu/DishList.js
+++ src/components/menu/DishList.js
@@ -1,42 +1,62 @@
-import { Typography } from "@material-ui/core";
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import { Avatar, ListItemAvatar, Typography } from "@material-ui/core";
+import React from "react";
 import { withStyles } from "@material-ui/core";
 import { compose } from "redux";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
+    paddingLeft: "2rem",
+    paddingRight: "2rem",
+    marginTop: "1rem",
   },
+  dishTitle: {
+    fontSize: "1.3rem",
+    fontStyle: "italic",
+  },
+  allergenList: {
+    display: "flex",
+  },
+ dishPrice: {
+   display: "flex",
+   justifyContent: "flex-end",
+ },
+ titleAllergenSection: {
+   marginTop: "1rem",
+ }
 });
 
-class DishList extends Component {
+const DishList = ({
+  classes,
+  title,
+  price,
+  ingredients,
+  description,
+  allergens,
+}) => {
+  return (
+    <div className={classes.root}>
+      <hr />
+      <Typography className={classes.dishTitle} variant="body1">{title}</Typography>
+      <Typography className={classes.dishPrice} variant="body1">{price}</Typography>
+      <Typography variant="body2">{ingredients}</Typography>
+      <Typography variant="body2">{description}</Typography>
 
-  render() {
-    const { classes, title, price, ingredients, description, allergens } = this.props;
+      <Typography variant="body2" className={classes.titleAllergenSection}>allergènes signalés :</Typography>
+      <div className={classes.allergenList}>
 
-    return (
-      <div className={classes.root}>
-      <hr/>
-        <Typography>{title}</Typography>
-        <Typography>{ingredients}</Typography>
-        <Typography>{description}</Typography>
-        <Typography>{allergens}</Typography>
-        <Typography>{price}</Typography>
-        <hr/>
+      {allergens.map((allergen, i) => 
+      <ListItemAvatar key={i}>
+        <Avatar
+        alt={`${allergen}`}
+        src={require(`../../assets/allergens/${allergen}.png`)}
+        />
+      </ListItemAvatar>
+      )}
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    dishes: state.firestore.ordered.dishes,
-  };
+      <hr />
+    </div>
+  );
 };
 
-export default compose(
-  withStyles(styles),
-  connect(mapStateToProps),
-)(DishList);
+export default compose(withStyles(styles))(DishList);
--- src/components/menu/MenuPage.js
+++ src/components/menu/MenuPage.js
@@ -29,12 +29,10 @@ const styles = (theme) => ({
   restoName: {
     marginTop: "3rem",
   },
-  menuContent: {
-
-  },
+  menuContent: {},
   menuTitle: {
     marginTop: "2rem",
-    textAlign: "center"
+    textAlign: "center",
   },
 });
 
@@ -46,49 +44,52 @@ class MenuPage extends Component {
     console.log("MMMMMMMMMMMMMMMMmenuId in componentDIdMount", menuId);
   }
 
-
-
   render() {
     const { classes, restaurant, menu, dishes } = this.props;
     const resto = { ...restaurant };
     console.log("++++++++RESTO IN MENUPAGE", resto);
     const menuData = { ...menu };
     console.log("+++++++++MENU IN MENUPAGE", menuData);
-
+    console.log("MMMMMMMMMMMMMMMMmenuData.title ", menuData.title);
 
     return (
       <div className={classes.root}>
+
         <div className={classes.menuHearder}>
           <Typography className={classes.restoName} variant="h1">
             {resto.name}
           </Typography>
           <img className={classes.logo} src={resto.logo} alt="logo" />
         </div>
+
         <div className={classes.restoContact}>
           <Typography variant="body1"> {resto.adress} </Typography>
           <Typography variant="body1">
-            {" "}
-            {resto.postalCode} {resto.city}{" "}
+            {" "}{resto.postalCode} {resto.city}{" "}
           </Typography>
           <Typography variant="body1"> {resto.phone} </Typography>
           <Typography variant="body1"> {resto.email} </Typography>
         </div>
+
         <div className={classes.menuContent}>
           <Typography variant="h1" className={classes.menuTitle}>
-            {resto.template === "template3" ? menuData.title : "La carte"}
+          {menuData.title}
           </Typography>
-          { dishes && dishes.map((dish) => (
-          <DishList 
-           key={dish.id}
-           menu={menuData}
-           title={dish.dishName}
-           price={dish.price}
-           ingredients={dish.ingredients}
-           description={dish.description}
-           allergens={dish.checkedAllergens}
-           />
-          ))}
+   
+          {dishes &&
+            dishes.map((dish) => (
+              <DishList
+                key={dish.id}
+                menu={menuData}
+                title={dish.dishName}
+                price={dish.price}
+                ingredients={dish.ingredients}
+                description={dish.description}
+                allergens={dish.checkedAllergens}
+              />
+            ))}
         </div>
+        
       </div>
     );
   }
@@ -100,9 +101,9 @@ const mapStateToProps = (state) => {
     restaurant:
       state.firestore.ordered.restaurants &&
       state.firestore.ordered.restaurants[0],
-      menu: state.firestore.ordered.menus && state.firestore.ordered.menus[0],
-      dishes: state.firestore.ordered.dishes
-    };
+    menu: state.firestore.ordered.menus && state.firestore.ordered.menus[0],
+    dishes: state.firestore.ordered.dishes,
+  };
 };
 
 export default compose(
