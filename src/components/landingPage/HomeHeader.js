6945119e109f0c3000e90b44d6cfe342c7c3b9bc
AnnaMarandola <annamarandola@gmail.com>
2021-01-05 11:11:11 +0100

    fixing free trial button position

--- src/components/landingPage/HomeHeader.js
+++ src/components/landingPage/HomeHeader.js
@@ -10,22 +10,22 @@ const styles = (theme) => ({
     display: "flex",
     flexDirection: "column-reverse",
     justifyContent: "space-around",
-    marginTop: "2rem",
-    [theme.breakpoints.up('sm')]: {
-        flexDirection: "row",
-        width: "80%",
-        marginLeft: "10%",
-    }
+    marginTop: "3rem",
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
+      width: "80%",
+      marginLeft: "10%",
+    },
   },
   headerTitle: {
     marginBottom: "2rem",
     marginTop: "-3rem",
     fontFamily: "Archivo narrow",
     fontSize: "4rem",
-    [theme.breakpoints.up('sm')]: {
-        marginTop: "5rem",
-        fontSize: "6rem",
-    }
+    [theme.breakpoints.up("sm")]: {
+      marginTop: "5rem",
+      fontSize: "6rem",
+    },
   },
   titleSpan: {
     color: "#ee1c80",
@@ -33,9 +33,9 @@ const styles = (theme) => ({
   headerText: {
     marginLeft: "1.5rem",
     marginRight: "1.5rem",
-    [theme.breakpoints.up('sm')]: {
-        marginTop: "5rem",
-    }
+    [theme.breakpoints.up("sm")]: {
+      marginTop: "5rem",
+    },
   },
   buttonSection: {
     marginTop: "2rem",
@@ -47,22 +47,26 @@ const styles = (theme) => ({
     marginBottom: "1rem",
     marginRight: "1rem",
     fontWeight: "600",
-    [theme.breakpoints.up('sm')]: {
-        marginBottom: "0",
-    }
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: "0",
+    },
   },
   registerButton: {
     backgroundColor: "#ee1c80",
     color: "white",
     fontWeight: "600",
+    marginBottom: "1rem",
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: "0",
+    },
   },
   headerImg: {
     width: "80%",
     marginLeft: "2rem",
-    [theme.breakpoints.up('sm')]: {
-        width: "100%",
-        margin: "0",
-    }
+    [theme.breakpoints.up("sm")]: {
+      width: "100%",
+      margin: "0",
+    },
   },
   links: {
     textDecoration: "none",
@@ -71,39 +75,38 @@ const styles = (theme) => ({
 
 const HomeHeader = ({ classes, auth }) => {
   return (
-      <div className={classes.header}>
-        <div className={classes.headerText}>
-          <Typography variant="h1" className={classes.headerTitle}>
-            <span className={classes.titleSpan}>QR</span>Menu
-          </Typography>
-          <Typography variant="h6">
-            {" "}
-            Digitalisez votre menu et donnez de la visibilité à votre restaurant
-            !
-          </Typography>
-          <Typography variant="h6">
-            {" "}
-            Crééz votre carte en ligne en quelques clics{" "}
-          </Typography>
-          <div className={classes.buttonSection}>
-            <Link to="./signin" className={classes.links} >
-              <Button className={classes.connectButton}>Connexion</Button>
-            </Link>
-            <Link className={classes.links} to="./createaccount">
-              <Button className={classes.registerButton}>
-                Essai gratuit de 14 jours
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div className={classes.headerImage}>
-          <img
-            src={HEADER}
-            className={classes.headerImg}
-            alt="illustration cuisinier et QR code"
-          />
+    <div className={classes.header}>
+      <div className={classes.headerText}>
+        <Typography variant="h1" className={classes.headerTitle}>
+          <span className={classes.titleSpan}>QR</span>Menu
+        </Typography>
+        <Typography variant="h6">
+          {" "}
+          Digitalisez votre menu et donnez de la visibilité à votre restaurant !
+        </Typography>
+        <Typography variant="h6">
+          {" "}
+          Crééz votre carte en ligne en quelques clics{" "}
+        </Typography>
+        <div className={classes.buttonSection}>
+          <Link to="./signin" className={classes.links}>
+            <Button className={classes.connectButton}>Connexion</Button>
+          </Link>
+          <Link className={classes.links} to="./createaccount">
+            <Button className={classes.registerButton}>
+              Essai gratuit de 14 jours
+            </Button>
+          </Link>
         </div>
       </div>
+      <div className={classes.headerImage}>
+        <img
+          src={HEADER}
+          className={classes.headerImg}
+          alt="illustration cuisinier et QR code"
+        />
+      </div>
+    </div>
   );
 };
 
