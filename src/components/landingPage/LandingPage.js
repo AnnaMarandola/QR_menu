400a9e866ad47af87229381bb1459daf970344f1
AnnaMarandola <annamarandola@gmail.com>
2021-01-05 15:36:04 +0100

    navigation anchors on landing page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,29 +1,87 @@
 import React from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
-import { withStyles } from "@material-ui/core";
+import { withStyles, Typography } from "@material-ui/core";
 import HomeHeader from "./HomeHeader";
-import HomeTopBar from "./HomeTopBar";
 import HowDoesItWork from "./HowDoesItWork";
 import Commitments from "./Commitments";
 import Pricing from "./Pricing";
+import AVATAR from "../../assets/landingPage/Sans titre (9).png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
+  },
+  rootNav: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      height: "5rem",
+      borderBottom: "1px solid grey",
+    },
+  },
+  homeNav: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+  },
+  menuItem: {
+    marginRight: "2rem",
+    fontFamily: "Archivo narrow",
+    fontSize: "1.5rem",
+    fontWeight: "300",
+    fontColor: "grey",
+  },
+  avatarImg: {
+    width: "3%",
+    marginRight: "3rem",
+  },
+  navlink: {
+    textDecoration: "none",
+    color: "inherit",
   },
 });
 
 const LandingPage = ({ classes, auth }) => {
   return (
     <div className={classes.root}>
-      <div className={classes.topbar}>
-        <HomeTopBar />
+      <div className={classes.rootNav}>
+        <div className={classes.homeNav}>
+          <AnchorLink href="#product" className={classes.navlink}>
+            <Typography className={classes.menuItem}>PRODUITS</Typography>
+          </AnchorLink>
+          <AnchorLink href="#pricing" className={classes.navlink}>
+            <Typography className={classes.menuItem}>ABONNEMENTS</Typography>
+          </AnchorLink>
+          <AnchorLink href="#engagements" className={classes.navlink}>
+            <Typography className={classes.menuItem}>
+              NOS ENGAGEMENTS
+            </Typography>
+          </AnchorLink>
+          {/* <AnchorLink href="#contact" className={classes.navlink}> */}
+            <Typography className={classes.menuItem}>CONTACT</Typography>
+          {/* </AnchorLink> */}
+          {/* <AnchorLink href="#shop" className={classes.navlink}> */}
+            <Typography className={classes.menuItem}>BOUTIQUE</Typography>
+          {/* </AnchorLink> */}
+          {/* <NavLink to="./signin"> */}
+          <img src={AVATAR} alt="avatar" className={classes.avatarImg} />
+          {/* </NavLink> */}
+        </div>
       </div>
+
       <HomeHeader />
-      <HowDoesItWork />
-      <Commitments />
-      <Pricing />
+      <section id="product">
+        <HowDoesItWork />
+      </section>
+      <section id="engagements">
+        <Commitments />
+      </section>
+      <section id="pricing">
+        <Pricing />
+      </section>
     </div>
   );
 };
