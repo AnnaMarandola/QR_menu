3e2a122deaf7d65ba8ef299e6c789be2ce98cc3b
AnnaMarandola <annamarandola@gmail.com>
2021-02-17 19:44:06 +0100

    coorection

--- src/components/dashboard/menu/DishItemEdit.js
+++ src/components/dashboard/menu/DishItemEdit.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Typography, Button, Card, Fab } from "@material-ui/core";
+import { Typography, Button, Card } from "@material-ui/core";
 import { withStyles } from "@material-ui/core";
 import { compose } from "redux";
 import { connect } from "react-redux";
